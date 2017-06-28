878caf1e1c0c5320693fc80331238119e1457c78
licong <licong19961204@gmail.com>
2017-06-28 22:20:36 +0800

    加了nav侧导航
    加了js和css

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,36 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <div id="perspective" class="perspective effect-rotateleft">
+			<div class="container" id="clicks">
+				<div class="wrapper"><!-- wrapper needed for scroll -->
+					<!-- Top Navigation -->
+					<div class="main clearfix">
+						<div class="123">
+              <p><button id="showMenu">Show Menu</button></p>
+              <router-view></router-view>
+						</div>
+					</div>
+				</div><!-- wrapper -->
+			</div><!-- /container -->
+			<nav class="outer-nav right vertical">
+				<a href="#" class="icon-home">首页</a>
+				<a href="#" class="icon-news">News</a>
+				<a href="#" class="icon-image">Images</a>
+				<a href="#" class="icon-upload">Uploads</a>
+				<a href="#" class="icon-star">Favorites</a>
+			</nav>
+		</div><!-- /perspective -->
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  export default {
+    name: 'app',
+    methods: {
+      coutert () {
+      }
+    }
+  }
 </script>
 
 <style lang="scss">
